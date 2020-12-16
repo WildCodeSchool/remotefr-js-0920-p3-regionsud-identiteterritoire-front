d60130e4b3b931c7711d1ad903e157d16051842b
Cyrille F <cyrille.fabre@gmail.com>
2020-12-16 12:31:03 +0100

    Adding some style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import logoQuadri from '../assets/LOGO-2-QUADRI.jpg';
+import logoWCS from '../assets/Home_LogoWCS.png';
 import headerBackground from '../assets/header.png';
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
     <header>
       <div className='header-logo'>
         <img className='logo-homepage' src={logoQuadri} alt='Logo Region Sud' />
+        <img className='logo-homepage' src={logoWCS} alt='Logo Region Sud' />
       </div>
       <div className='header'>
         <img

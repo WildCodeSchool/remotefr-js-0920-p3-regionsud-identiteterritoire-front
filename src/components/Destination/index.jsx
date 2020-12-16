d0785c25978f2cf4808534941d71f36738c66572
Miguel Santos <migueljsantos27@gmail.com>
2020-12-16 14:14:36 +0100

    replace all p to h4

--- src/components/Destination/index.jsx
+++ src/components/Destination/index.jsx
@@ -1,37 +1,40 @@
 // eslint-disable react/prefer-stateless-function
 // eslint-disable react/no-unescaped-entities
 import React from 'react';
+import './destination.css';
 
 export default function Destination() {
   return (
     <div className="row">
-      <div className="col-md-3">
+      <div className="col-md-4">
         <img
           src="https://cdn-media.rtl.fr/cache/FK9y_F2KrJv76B7pwcilKg/880v587-0/online/image/2012/0704/7750350513_carte-de-localisation-de-toulon-var.jpg"
           className="img-fluid"
           alt="Responsive_imge"
         />
-        <p>
-          Population: 166 700 <small>(2008)</small>
-        </p>
+        <div className="text_1">
+          <h4>
+            Population: 166 700 <small>(2008)</small>
+          </h4>
 
-        <p>Superficie: 42,84 km²</p>
-        <p>Maire: Hubert Falco</p>
+          <h4>Superficie: 42,84 km²</h4>
+          <h4>Maire: Hubert Falco</h4>
+        </div>
       </div>
-      <div className="col-md-9">
-        <p className="text-parag">
+      <div className="col-md-8">
+        <h4 className="text-parag">
           Toulon est une commune du sud-Est de la France, chef-lieu du
           département du Var et siège de sa préfecture.
-        </p>
-        <p>
+        </h4>
+        <h4>
           Troisième ville de la région Provence-Alpes-Côte d'Azur derrière
           Marseille et Nice,elle abrite en outre le siège de la préfecture
           maritime de la Méditerranée.
-        </p>
-        <p>
+        </h4>
+        <h4>
           La commune est établie sur les bords de la mer Méditerranée, le long
           de la rade de Toulon. Ses habitants sont appelés les Toulonnais.
-        </p>
+        </h4>
       </div>
     </div>
   );

7634600def1e6d2fc1416eb3fcb8a69b46ff7232
Miguel Santos <migueljsantos27@gmail.com>
2020-12-16 11:46:31 +0100

    add component destination and add index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Templates from './components/templates';
 import Commune from './components/pages/Commune'; // Exemple
 import Home from './components/pages/Home'; // Exemple
+import Destination from './components/Destination';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Templates>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/destination" component={Destination} />
           <Route exact path="/commune/:id" component={Commune} />
         </Switch>
       </Templates>

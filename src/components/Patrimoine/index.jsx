0a1ad6bf103c3f9da28d8124f7980fa4c2b66c5c
Miguel Santos <migueljsantos27@gmail.com>
2021-01-22 09:49:36 +0100

    es-lint debug

--- src/components/Patrimoine/index.jsx
+++ src/components/Patrimoine/index.jsx
@@ -11,6 +11,25 @@ function Patrimoine() {
           <h1 className="title_p">
             <b>Patrimoine</b>
           </h1>
+          <div className="col-md-4 rounded  float-right">
+            <img
+              src={logoMusee}
+              className="img-fluid img-thumbnail roundedImage"
+              alt="Responsive_imge"
+            />
+            <p>
+              <b>Musée d&apos;art</b>
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              incidunt quis id ex vel? Iste perferendis excepturi porro id illo
+              pariatur in voluptatem sunt, reprehenderit nulla dolores?
+              Dignissimos, deleniti culpa!
+            </p>
+          </div>
+        </div>
+        <div className="col-md-6">
           <h4>
             <b>Musées</b>
           </h4>
@@ -24,34 +43,21 @@ function Patrimoine() {
             <br /> par l&apos;Etat.Quelques instituitions, non
             <br /> porteuses de l&apos;appellation musée de
             <br /> France, comme celles relavant de
-            <br /> l&apos;institut de France, figurent dans ce recensement.
+            <br /> l&apos;institut de France, figurent dans ce <br />
+            recensement.
           </p>
+        </div>
+        <div className="col-md-3">
           <img
             src={logoMinistere}
-            className="img-rate-quality-picto logoMinister "
+            className="img-rate-quality-pico logoMinister"
             alt="Responsive_imge"
           />
         </div>
-        <div className="Row">
-          <div className="col-md-6">
-            <img
-              src={logoMusee}
-              className="img-rate-quality-picto  roundedImage"
-              alt="Responsive_imge"
-            />
-          </div>
-          <div className="col-md-6">
-            <p>
-              <b>Musée d&apos;art</b>
-            </p>
-
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              incidunt quis id ex vel? Iste perferendis excepturi porro id illo
-              pariatur in voluptatem sunt, reprehenderit nulla dolores?
-              Dignissimos, deleniti culpa!
-            </p>
-          </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <hr className="orange-bar" />
         </div>
       </div>
     </div>
